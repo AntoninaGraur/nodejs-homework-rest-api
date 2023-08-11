d16bfd23b4104e36248fe67e43226e75d87d8281
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-08-11 13:06:34 +0200

    ENV CONFIG

--- helpers/Mailer.js
+++ helpers/Mailer.js
@@ -3,3 +3,15 @@ import dotenv from "dotenv";
 
 
 dotenv.config();
+
+const { MAIL_PASSWORD } = process.env;
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.ukr.net",
+    port: 567,
+    secure: true,
+    auth: {
+        user: "graurtosia@gmail.com",
+        pass: MAIL_PASSWORD,
+    }
+})
